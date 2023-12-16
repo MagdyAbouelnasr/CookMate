3cd5e2ec93b2dcd87108bc365755dd3fa96b14b7
MagdyAbouelnasr <mmabouelnasr@gmail.com>
2023-12-16 21:38:21 +0200

    future activate email functionality

--- app/activate/[token]/route.ts
+++ app/activate/[token]/route.ts
@@ -1,55 +1,56 @@
-// import { redirect } from "next/navigation";
+import { redirect } from "next/navigation";
+import prisma from "@/lib/prisma";
 
-// export async function GET(
-//   request: Request,
-//   { params }: { params: { token: string } }
-// ) {
-//   const { token } = params;
+export async function GET(
+  request: Request,
+  { params }: { params: { token: string } }
+) {
+  const { token } = params;
 
-//   const user = await prisma?.user.findFirst({
-//     where: {
-//       activateTokens: {
-//         some: {
-//           AND: [
-//             {
-//               activatedAt: null,
-//             },
-//             {
-//               createdAt: {
-//                 gt: new Date(Date.now() - 24 * 60 * 60 * 1000),
-//               },
-//             },
-//             {
-//               token,
-//             },
-//           ],
-//         },
-//       },
-//     },
-//   });
+  const user = await prisma?.user.findFirst({
+    where: {
+      activateTokens: {
+        some: {
+          AND: [
+            {
+              activatedAt: null,
+            },
+            {
+              createdAt: {
+                gt: new Date(Date.now() - 24 * 60 * 60 * 1000),
+              },
+            },
+            {
+              token,
+            },
+          ],
+        },
+      },
+    },
+  });
 
-//   if (!user) {
-//     throw new Error("Invalid token");
-//   }
+  if (!user) {
+    throw new Error("Invalid token");
+  }
 
-//   await prisma?.user.update({
-//     where: {
-//       id: user.id,
-//     },
-//     data: {
-//       active: true,
-//     },
-//   });
+  await prisma?.user.update({
+    where: {
+      id: user.id,
+    },
+    data: {
+      active: true,
+    },
+  });
 
 
-//   await prisma.activateToken.update({
-//     where: {
-//         token,
-//     },
-//     data: {
-//         activatedAt: new Date()
-//     }
-//   })
+  await prisma.activateToken.update({
+    where: {
+        token,
+    },
+    data: {
+        activatedAt: new Date()
+    }
+  })
 
-//   redirect('/auth/sigmIn')
-// }
+  redirect('/auth/sinIn')
+}
